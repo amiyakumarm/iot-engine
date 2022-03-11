18793076c79e6a29e7ec49850e0dcddd62d122d1
amiyakumarm <88655714+amiyakumarm@users.noreply.github.com>
2022-03-11 14:14:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# iot-engine
+ 
---
+++ src/test/java/com/iotengine/IotEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iotengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
